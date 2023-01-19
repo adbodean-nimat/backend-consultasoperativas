ad23c2e89f7bac9a496d5455f76513e975f3de03
Antonio Bodean <abodean@prades.local>
2023-01-19 17:09:43 -0300

    Ultima actualización

---
+++ server/fsconfig.js
@@ -0,0 +1,9 @@
+const { replace } = require('lodash');
+const xlsx = require('xlsx');
+
+exports.getFileExcel = async (request, response) => {
+    const inputFilePath = 'file://192.168.0.46/prades_fs/Departamento Ventas/VENTAS TABLEROS y CONSULTAS RAPIDAS/EGE/ROWA PLAN CANJE POR SIEMPRE ROWA.xlsx'
+    let File = xlsx.readFile(new URL(inputFilePath));
+    let Content = xlsx.utils.sheet_to_json(File.Sheets[File.SheetNames[0]]);
+    response.status(200).json(JSON.parse(JSON.stringify(Content).replace(/, /g, ' ').replace(/-/g, ' ').replace(/"\s+|\s+"/g,'"').replace(/(\s+)(?=[(\w* *]*":)/g, "_")))
+}
