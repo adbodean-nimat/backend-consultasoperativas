39b8e4d2759a5791a7b5fd845ee578f405606a5a
Antonio Bodean <antonio.bodean@nimat.com.ar>
2025-05-07 15:21:38 -0300

    Cambiar logo NIMAT

--- server/fsconfig.js
+++ server/fsconfig.js
@@ -1,7 +1,7 @@
 const xlsx = require('xlsx');
 
 exports.getFileExcel = async (request, response) => {
-    const inputFilePath = '/mnt/prades_fs/ventas/ROWA PLAN CANJE POR SIEMPRE ROWA.xlsx'
+    const inputFilePath = process.env.URL_DIR_EGE
     let File = xlsx.readFile(inputFilePath);
     let Content = xlsx.utils.sheet_to_json(File.Sheets[File.SheetNames[0]]);
     response.status(200).json(JSON.parse(JSON.stringify(Content).replace(/, /g, ' ').replace(/-/g, ' ').replace(/"\s+|\s+"/g,'"').replace(/(\s+)(?=[(\w* *]*":)/g, "_")))
