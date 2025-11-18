50f95939afbfe9dd0a7b6fd82778ec7644b16d8b
Antonio Bodean <antonio.bodean@nimat.com.ar>
2025-11-18 16:03:08 -0300

    Restablecer "fsconfig.js"

--- server/fsconfig.js
+++ server/fsconfig.js
@@ -1,13 +1,13 @@
-import xlsx from 'xlsx';
+const xlsx = require('xlsx');
 
-export async function getFileExcel(request, response) {
+exports.getFileExcel = async (request, response) => {
     const inputFilePath = process.env.URL_DIR_EGE
     let File = xlsx.readFile(inputFilePath);
     let Content = xlsx.utils.sheet_to_json(File.Sheets[File.SheetNames[0]]);
     response.status(200).json(JSON.parse(JSON.stringify(Content).replace(/, /g, ' ').replace(/-/g, ' ').replace(/"\s+|\s+"/g,'"').replace(/(\s+)(?=[(\w* *]*":)/g, "_")))
 }
 
-export async function getLogEnviado(request, response) {
+exports.getLogEnviado = async (request, response) => {
     const inputFilePath = process.env.URL_DIR_LOGS
     const workbook = xlsx.readFile(inputFilePath + '/envios_ok.csv');
     const SheetNames = workbook.SheetNames[0];
@@ -16,7 +16,7 @@ export async function getLogEnviado(request, response) {
     response.status(200).json(JSON.parse(JSON.stringify(Content)))
 }
 
-export async function getLogError(request, response) {
+exports.getLogError = async (request, response) => {
     const inputFilePath = process.env.URL_DIR_LOGS
     const workbook = xlsx.readFile(inputFilePath + '/envios_error.csv');
     const SheetNames = workbook.SheetNames[0];
