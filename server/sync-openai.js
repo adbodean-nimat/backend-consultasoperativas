15db8da26b6c51db19a28b77b776382d0b29ff14
Antonio Bodean <antonio.bodean@nimat.com.ar>
2025-12-29 14:30:54 -0300

    Habilitar log

--- server/sync-openai.js
+++ server/sync-openai.js
@@ -69,29 +69,29 @@ export async function syncOpenAI() {
     process.exit(1);
   }
 
-  //console.log(`📂 Vector store: ${VECTOR_STORE_ID}`);
-  //console.log(`📄 Archivos local: ${LOCAL_FILE_PATH_PRODUCTOS}, ${LOCAL_FILE_PATH_FAQ}, ${LOCAL_FILE_PATH_INFO}`);
+  console.log(`📂 Vector store: ${VECTOR_STORE_ID}`);
+  console.log(`📄 Archivos local: ${LOCAL_FILE_PATH_PRODUCTOS}, ${LOCAL_FILE_PATH_FAQ}, ${LOCAL_FILE_PATH_INFO}`);
 
   // 1) Buscar si ya existe un producto.json en el vector store (por filename real)
-  //console.log("🔍 Buscando productos.json/faq.md/nimat_conocimiento_general.md existenten en el vector store...");
+  console.log("🔍 Buscando productos.json/faq.md/nimat_conocimiento_general.md existenten en el vector store...");
   const existing = await findExistingFileByFilename(VECTOR_STORE_ID, TARGET_FILE_NAME_PRODUCTOS);
   const existingFAQ = await findExistingFileByFilename(VECTOR_STORE_ID, TARGET_FILE_NAME_FAQ);
   const existingINFO = await findExistingFileByFilename(VECTOR_STORE_ID, TARGET_FILE_NAME_INFO);
 
   if (existing && existingFAQ && existingINFO) {
-    /* console.log(
+     console.log(
       `Encontrado: 
         Producto: ${existing.filename} (vectorStoreFileId: ${existing.vectorStoreFileId}, fileId: ${existing.fileId})\n
         FAQ: ${existingFAQ.filename} (vectorStoreFileId: ${existingFAQ.vectorStoreFileId}, fileId: ${existingFAQ.fileId})\n
         INFO: ${existingINFO.filename} (vectorStoreFileId: ${existingINFO.vectorStoreFileId}, fileId: ${existingINFO.fileId})\n
       `
-    ); */
+    );
   } else {
     console.log("No hay archivos previo en el vector store.");
   }
 
   // 2) Subir nuevo archivo a Files API
-  //console.log("⬆️ Subiendo nuevo archivos a Files API...");
+  console.log("⬆️ Subiendo nuevo archivos a Files API...");
   const uploadedFile = await client.files.create({
     file: fs.createReadStream(LOCAL_FILE_PATH_PRODUCTOS),
     purpose: "assistants",
@@ -106,7 +106,7 @@ export async function syncOpenAI() {
     purpose: "assistants"
   });
 
-  //console.log(`Nuevo files subido. fileId: ${uploadedFile.id}, ${uploadedFileFAQ.id}, ${uploadedFileINFO.id}`);
+  console.log(`Nuevo files subido. fileId: ${uploadedFile.id}, ${uploadedFileFAQ.id}, ${uploadedFileINFO.id}`);
 
   // 3) Asociar el nuevo file al vector store
   //console.log("📌 Asociando nuevo files al vector store...");
@@ -120,27 +120,21 @@ export async function syncOpenAI() {
     file_id: uploadedFileINFO.id,
   });
 
-  //console.log("✅ Asociado correctamente.");
+  console.log("✅ Asociado correctamente.");
 
   // 4) (Opcional) Eliminar el vínculo viejo del vector store
   if (existing?.vectorStoreFileId && existingFAQ?.vectorStoreFileId && existingINFO?.vectorStoreFileId) {
-      //console.log("🧹 Eliminando archivo viejo del vector store...");
+      console.log("🧹 Eliminando archivo viejo del vector store...");
       await client.vectorStores.files.delete(existing.vectorStoreFileId, {vector_store_id: VECTOR_STORE_ID});
       await client.vectorStores.files.delete(existingFAQ.vectorStoreFileId, {vector_store_id: VECTOR_STORE_ID});
       await client.vectorStores.files.delete(existingINFO.vectorStoreFileId, {vector_store_id: VECTOR_STORE_ID});
-      //console.log("Viejo vínculo eliminado del vector store.");
-      //console.log("🧹 Eliminando archivos viejo");
+      console.log("Viejo vínculo eliminado del vector store.");
+      console.log("🧹 Eliminando archivos viejo");
       await client.files.delete(existing.fileId);
       await client.files.delete(existingFAQ.fileId);
       await client.files.delete(existingINFO.fileId);
-      //console.log("Archivos viejo eliminado")
+      console.log("Archivos viejo eliminado")
   }
 
-  //console.log("🎉 Listo: vector store actualizado");
-  //console.log("🎉 Listo: archivos viejo eliminado");
+  console.log("🎉 Listo: vector store actualizado y archivos eliminados");
 }
-
-/* main().catch((err) => {
-  console.error("Error actualizando el vector store:", err.response?.data ?? err);
-  process.exit(1);
-}); */
